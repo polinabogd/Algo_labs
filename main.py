357fabff585482fdc89fbd16a014a0e50740f0b1
polinabogd <74895012+polinabogd@users.noreply.github.com>
2021-10-19 14:57:55 +0300

    adding method counting the discount

--- main.py
+++ main.py
@@ -11,5 +11,17 @@ def sorting_prices(list_of_prices: list, discount_pers):
     for i in range(len(list_of_prices) // 3):
         max_el_in_end = list_of_prices.pop(-1)
         max_el_in_end = max_el_in_end - (max_el_in_end * discount_pers)
-        list_of_prices.insert((i*3+2), round(max_el_in_end, 2))
+        list_of_prices.insert((i * 3 + 2), round(max_el_in_end, 2))
     return list_of_prices
+
+
+def counting_discount():
+    input_data_prices = open_and_read_file("file.txt")
+    discount_pers = (input_data_prices.pop(-1) / 100)
+    sorted_prices_list = sorting_prices(input_data_prices, discount_pers)
+    final_sum = sum(sorted_prices_list)
+    return final_sum
+
+
+if __name__ == '__main__':
+    print(counting_discount())
