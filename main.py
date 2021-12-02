f9e8d1c5aaaaa94cbe75a0ce61287198b75fb661
polinabogd <74895012+polinabogd@users.noreply.github.com>
2021-12-02 18:12:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
-length, inner_length = 5, 5
-matrix = [[0, 0, 0, -1, 0],
-           [-1, 0, 0, -1, -1],
-           [0, 0, 0, -1, 0],
-           [-1, 0, -1, 0, -1],
-           [0, 0, -1, 0, 0]]
+length, inner_length = 6, 7
+matrix = [
+            ["a", "a", "a", "a", "a", "a", "a"],
+            ["a", "a", "a", "a", "a", "a", "a"],
+            ["a", "a", "a", "a", "a", "a", "a"],
+            ["a", "a", "a", "a", "a", "a", "a"],
+            ["a", "a", "a", "a", "a", "a", "a"],
+            ["a", "a", "a", "a", "a", "a", "a"]
+        ]
 
 
-def go_through():
-    counter = 0
-    for n in range(length):
-        counter += 1
-
-    n, m = 0, 0
-    while m < inner_length and n < length:
-        counter += find_ways_right(m, n)
-        counter += find_ways_cross(m, n)
-        m += 1
-        n += 1
+def go_through(out, inn, counter):
+    while out < length and inn < inner_length - 1:
+        p = matrix[out][inn+2:]
+        l = len(p)
+        for el in range(len(p)-1):
+            if p[el] == matrix[out][inn]:
+                k = el+inn+2
+                counter += 1
+                go_through(out, k, counter)
 
 
+        if out != 0:
+            if matrix[out][inn] == matrix[out-1][inn+1]:
+                counter += 1
+                go_through(out-1, inn+1, counter)
+        if out < length-1:
+            if matrix[out][inn] == matrix[out+1][inn+1]:
+                counter += 1
+                go_through(out+1, inn+1, counter)
+        return counter
 
-def find_ways_right(m, n):
-    c = 0
-    for t in matrix[n]:
-        if t == matrix[n][m]:
-            c += 1
-    return c
 
-
-def find_ways_cross(m, n):
-    c = 0
-    if matrix[n][m] == matrix[n + 1][m + 1]:
-        c += 1
-    elif matrix[n][m] == matrix[n - 1][m + 1]:
-        c += 1
-    return c
+def result():
+    if length == 1:
+        counter = 1
+        result = go_through(0, 0, counter)
+    else:
+        counter = 2
+        result = go_through(length-1, 0, counter) + go_through(0, 0, counter)
+    return result
 
 
 if __name__ == '__main__':
-    print(go_through())
+    print(result())
