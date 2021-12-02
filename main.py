3d4958a77fa1108b9097a5c90cafc569f6158fa9
polinabogd <74895012+polinabogd@users.noreply.github.com>
2021-12-02 19:36:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
-length, inner_length = 6, 7
-matrix = [
-            ["a", "a", "a", "a", "a", "a", "a"],
-            ["a", "a", "a", "a", "a", "a", "a"],
-            ["a", "a", "a", "a", "a", "a", "a"],
-            ["a", "a", "a", "a", "a", "a", "a"],
-            ["a", "a", "a", "a", "a", "a", "a"],
-            ["a", "a", "a", "a", "a", "a", "a"]
-        ]
+def search_ways(start_indexes: list, matrix: [[]]):
+    idx_amount = len(start_indexes)
+    for indexes in start_indexes:
+        left_idx = find_left_indexes(indexes, matrix)
+        if len(left_idx) > 0:
+            idx_amount += search_ways(left_idx, matrix) - 1
+    return idx_amount
 
 
-def go_through(out, inn, counter):
-    while out < length and inn < inner_length - 1:
-        p = matrix[out][inn+2:]
-        l = len(p)
-        for el in range(len(p)-1):
-            if p[el] == matrix[out][inn]:
-                k = el+inn+2
-                counter += 1
-                go_through(out, k, counter)
+def find_left_indexes(right_idx: tuple, matrix: list):
+    value = matrix[right_idx[0]][right_idx[1]]
+    left_indexes = []
+    for outer_idx in range(len(matrix)):
+        for inner_idx in range(right_idx[1]):
+            if matrix[outer_idx][inner_idx] == value:
+                left_indexes.append((outer_idx, inner_idx))
+    if right_idx[1] != 0 and matrix[right_idx[0]][right_idx[1] - 1] != value:
+        left_indexes.append((right_idx[0], right_idx[1] - 1))
+    return left_indexes
 
 
-        if out != 0:
-            if matrix[out][inn] == matrix[out-1][inn+1]:
-                counter += 1
-                go_through(out-1, inn+1, counter)
-        if out < length-1:
-            if matrix[out][inn] == matrix[out+1][inn+1]:
-                counter += 1
-                go_through(out+1, inn+1, counter)
-        return counter
+def result(matrix, length, inner_length):
+    counter = 0
+    if len(matrix) == 1:
+        counter = search_ways([(0, inner_length - 1)], matrix)
+    elif len(matrix) > 1:
+        counter = search_ways([(0, inner_length - 1), (length - 1, width - 1)], matrix)
 
-
-def result():
-    if length == 1:
-        counter = 1
-        result = go_through(0, 0, counter)
-    else:
-        counter = 2
-        result = go_through(length-1, 0, counter) + go_through(0, 0, counter)
-    return result
+    return counter
 
 
 if __name__ == '__main__':
-    print(result())
+    ijones_matrix = open("ijones.in", 'r').read().split('\n')[1:]
+    width = len(ijones_matrix[0])
+    height = len(ijones_matrix)
+    print(result(ijones_matrix,height, width))
